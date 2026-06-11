b896de7915f857b869b1abfa51f86bc9011f7da3
senoptick <kepvin00@gmail.com>
2026-06-11 14:24:57 +0300

    qwe

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     PlcNetwork *plc = PlcNetwork::instance();
-    plc -> connectToPlc("192.168.58.88", 1000);
+    plc -> connectToPlc("169.254.37.12", 1000);
     MainWindow w;
     w.showFullScreen();
     return a.exec();
