5703f9bd0bb0188eebe67e77a733521c2c572294
senoptick <kepvin00@gmail.com>
2026-06-11 12:41:21 +0300

    asd

---
+++ build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/opori_autogen/EWIEGA46WW/moc_plc_network.cpp
@@ -0,0 +1,207 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'plc_network.h'
+**
+** Created by: The Qt Meta Object Compiler version 69 (Qt 6.10.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../../plc_network.h"
+#include <QtCore/qmetatype.h>
+
+#include <QtCore/qtmochelpers.h>
+
+#include <memory>
+
+
+#include <QtCore/qxptype_traits.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'plc_network.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 69
+#error "This file was generated using the moc from 6.10.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+struct qt_meta_tag_ZN10PlcNetworkE_t {};
+} // unnamed namespace
+
+template <> constexpr inline auto PlcNetwork::qt_create_metaobjectdata<qt_meta_tag_ZN10PlcNetworkE_t>()
+{
+    namespace QMC = QtMocConstants;
+    QtMocHelpers::StringRefStorage qt_stringData {
+        "PlcNetwork",
+        "connected",
+        "",
+        "disconnected",
+        "dataReceived",
+        "data",
+        "errorOccurred",
+        "error",
+        "statusChanged",
+        "status",
+        "onSocketConnected",
+        "onSocketDisconnected",
+        "onSocketReadyRead",
+        "onSocketError",
+        "QAbstractSocket::SocketError",
+        "socketError"
+    };
+
+    QtMocHelpers::UintData qt_methods {
+        // Signal 'connected'
+        QtMocHelpers::SignalData<void()>(1, 2, QMC::AccessPublic, QMetaType::Void),
+        // Signal 'disconnected'
+        QtMocHelpers::SignalData<void()>(3, 2, QMC::AccessPublic, QMetaType::Void),
+        // Signal 'dataReceived'
+        QtMocHelpers::SignalData<void(const QByteArray &)>(4, 2, QMC::AccessPublic, QMetaType::Void, {{
+            { QMetaType::QByteArray, 5 },
+        }}),
+        // Signal 'errorOccurred'
+        QtMocHelpers::SignalData<void(const QString &)>(6, 2, QMC::AccessPublic, QMetaType::Void, {{
+            { QMetaType::QString, 7 },
+        }}),
+        // Signal 'statusChanged'
+        QtMocHelpers::SignalData<void(const QString &)>(8, 2, QMC::AccessPublic, QMetaType::Void, {{
+            { QMetaType::QString, 9 },
+        }}),
+        // Slot 'onSocketConnected'
+        QtMocHelpers::SlotData<void()>(10, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'onSocketDisconnected'
+        QtMocHelpers::SlotData<void()>(11, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'onSocketReadyRead'
+        QtMocHelpers::SlotData<void()>(12, 2, QMC::AccessPrivate, QMetaType::Void),
+        // Slot 'onSocketError'
+        QtMocHelpers::SlotData<void(QAbstractSocket::SocketError)>(13, 2, QMC::AccessPrivate, QMetaType::Void, {{
+            { 0x80000000 | 14, 15 },
+        }}),
+    };
+    QtMocHelpers::UintData qt_properties {
+    };
+    QtMocHelpers::UintData qt_enums {
+    };
+    return QtMocHelpers::metaObjectData<PlcNetwork, qt_meta_tag_ZN10PlcNetworkE_t>(QMC::MetaObjectFlag{}, qt_stringData,
+            qt_methods, qt_properties, qt_enums);
+}
+Q_CONSTINIT const QMetaObject PlcNetwork::staticMetaObject = { {
+    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
+    qt_staticMetaObjectStaticContent<qt_meta_tag_ZN10PlcNetworkE_t>.stringdata,
+    qt_staticMetaObjectStaticContent<qt_meta_tag_ZN10PlcNetworkE_t>.data,
+    qt_static_metacall,
+    nullptr,
+    qt_staticMetaObjectRelocatingContent<qt_meta_tag_ZN10PlcNetworkE_t>.metaTypes,
+    nullptr
+} };
+
+void PlcNetwork::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    auto *_t = static_cast<PlcNetwork *>(_o);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        switch (_id) {
+        case 0: _t->connected(); break;
+        case 1: _t->disconnected(); break;
+        case 2: _t->dataReceived((*reinterpret_cast<std::add_pointer_t<QByteArray>>(_a[1]))); break;
+        case 3: _t->errorOccurred((*reinterpret_cast<std::add_pointer_t<QString>>(_a[1]))); break;
+        case 4: _t->statusChanged((*reinterpret_cast<std::add_pointer_t<QString>>(_a[1]))); break;
+        case 5: _t->onSocketConnected(); break;
+        case 6: _t->onSocketDisconnected(); break;
+        case 7: _t->onSocketReadyRead(); break;
+        case 8: _t->onSocketError((*reinterpret_cast<std::add_pointer_t<QAbstractSocket::SocketError>>(_a[1]))); break;
+        default: ;
+        }
+    }
+    if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType(); break;
+        case 8:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType(); break;
+            case 0:
+                *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType::fromType< QAbstractSocket::SocketError >(); break;
+            }
+            break;
+        }
+    }
+    if (_c == QMetaObject::IndexOfMethod) {
+        if (QtMocHelpers::indexOfMethod<void (PlcNetwork::*)()>(_a, &PlcNetwork::connected, 0))
+            return;
+        if (QtMocHelpers::indexOfMethod<void (PlcNetwork::*)()>(_a, &PlcNetwork::disconnected, 1))
+            return;
+        if (QtMocHelpers::indexOfMethod<void (PlcNetwork::*)(const QByteArray & )>(_a, &PlcNetwork::dataReceived, 2))
+            return;
+        if (QtMocHelpers::indexOfMethod<void (PlcNetwork::*)(const QString & )>(_a, &PlcNetwork::errorOccurred, 3))
+            return;
+        if (QtMocHelpers::indexOfMethod<void (PlcNetwork::*)(const QString & )>(_a, &PlcNetwork::statusChanged, 4))
+            return;
+    }
+}
+
+const QMetaObject *PlcNetwork::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *PlcNetwork::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_staticMetaObjectStaticContent<qt_meta_tag_ZN10PlcNetworkE_t>.strings))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int PlcNetwork::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 9)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 9;
+    }
+    if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 9)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 9;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void PlcNetwork::connected()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
+}
+
+// SIGNAL 1
+void PlcNetwork::disconnected()
+{
+    QMetaObject::activate(this, &staticMetaObject, 1, nullptr);
+}
+
+// SIGNAL 2
+void PlcNetwork::dataReceived(const QByteArray & _t1)
+{
+    QMetaObject::activate<void>(this, &staticMetaObject, 2, nullptr, _t1);
+}
+
+// SIGNAL 3
+void PlcNetwork::errorOccurred(const QString & _t1)
+{
+    QMetaObject::activate<void>(this, &staticMetaObject, 3, nullptr, _t1);
+}
+
+// SIGNAL 4
+void PlcNetwork::statusChanged(const QString & _t1)
+{
+    QMetaObject::activate<void>(this, &staticMetaObject, 4, nullptr, _t1);
+}
+QT_WARNING_POP
---
+++ build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/opori_autogen/include/ui_mainwindow.h
@@ -0,0 +1,112 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.10.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(1920, 1080);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName("pushButton");
+        pushButton->setGeometry(QRect(250, 450, 361, 191));
+        pushButton->setFocusPolicy(Qt::FocusPolicy::NoFocus);
+        pushButton->setStyleSheet(QString::fromUtf8("QPushButton {\n"
+"background-image: url(:/Resources/start_normal.png);\n"
+"background-repeat: no-repeat;\n"
+"background-position: center;\n"
+"border: none;\n"
+"outline: none;\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"background-image: url(:/Resources/start_pressed.png);\n"
+"background-repeat: no-repeat;\n"
+"background-position: center;\n"
+"border: none;\n"
+"outline: none;\n"
+"}\n"
+"\n"
+""));
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName("pushButton_2");
+        pushButton_2->setEnabled(true);
+        pushButton_2->setGeometry(QRect(1309, 450, 361, 191));
+        pushButton_2->setMouseTracking(false);
+        pushButton_2->setFocusPolicy(Qt::FocusPolicy::NoFocus);
+        pushButton_2->setStyleSheet(QString::fromUtf8("QPushButton {\n"
+"background-image: url(:/Resources/stop_normal.png);\n"
+"background-repeat: no-repeat;\n"
+"background-position: center;\n"
+"border: none;\n"
+"outline: none;\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"background-image: url(:/Resources/stop_pressed.png);\n"
+"background-repeat: no-repeat;\n"
+"background-position: center;\n"
+"border: none;\n"
+"outline: none;\n"
+"}\n"
+"\n"
+""));
+        pushButton_2->setFlat(false);
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 1920, 22));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName("statusbar");
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        pushButton->setText(QString());
+        pushButton_2->setText(QString());
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
